df621d50ce95ba4f6becc37d48d2ee88c875894b
J L Gatewood <jlgatewood@gmail.com>
2021-02-14 14:05:46 +0900

    initial commit

---
+++ simple_calculator.py
@@ -0,0 +1,10 @@
+# SIMPLE ADDITION PROGRAM
+
+print("whats the first number?  ")
+x = input()
+
+# print("whats the second number?  ")
+y = input()
+
+print("your answer is:")
+print(int(x) + int(y))
