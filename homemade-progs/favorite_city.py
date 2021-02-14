6b528dce50f643917215674320780f11c25b18a3
J L Gatewood <StarrWulfe@users.noreply.github.com>
2021-02-14 19:05:31 +0900

    Update favorite_city.py
    forgot to save the recent version from another folder.

--- homemade-progs/favorite_city.py
+++ homemade-progs/favorite_city.py
@@ -1,13 +1,16 @@
+# VISIT YOKOHAMA! What's your fav city in the world?
+
 name = input("What city do you live in? ")
 
 def favorite_city(name):
     if (name == "Yokohama"):
-        print("Oh -- my favorite city is ", name,".")
-        print(name," is Japan's second largest city,")
+        print("Hey, that's my favorite city!")
+        print("Did you know that "name," is Japan's second largest city?")
         print("Please visit", name, "whenever you find a chance!")
 
     else:
         print("Well ", name," may be a fine place, but I prefer Yokohama.")
+        print("But I'll put that in my bucket list and visit there soon!")
 
 
 favorite_city (name)
